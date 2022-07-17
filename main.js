6567587cb97e52097dcbb4c99dc0f48bcfb31b29
baraa osama <baraaosama038@gmail.com>
2022-07-17 14:49:20 +0200

    this is first assignment in from 084 to 093 assignments

---
+++ main.js
@@ -0,0 +1,7 @@
+let imgarrays = document.images;
+
+for (i = 0; i < imgarrays.length; i++) {
+    document.images[i].src = "https://elzero.org/wp-content/themes/elzero/imgs/logo.png";
+    document.images[i].alt = "Elzero logo"
+}
+
