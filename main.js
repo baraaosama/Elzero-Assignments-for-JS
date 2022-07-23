4c7f023af3d7f48f0064f01d25dc10c526d064ef
baraa osama <baraaosama038@gmail.com>
2022-07-23 15:26:58 +0200

    First assignment from 94 to 101

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-let count = document.images;
-for (i = 0; i < count.length; i++) {
-    if (document.images[i].hasAttribute("alt")) {
-        document.images[i].setAttribute("alt", "Old")
-    } else {
-        document.images[i].setAttribute("alt", "Elzero New")
-    }
+let a = document.links[1];
+console.log(a)
+window.onload = function () {
+    a.click();
 }
