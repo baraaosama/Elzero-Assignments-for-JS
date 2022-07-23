5c1ecc95189686cfd2b4b214696b0db8e0deb8e7
baraa osama <baraaosama038@gmail.com>
2022-07-23 19:43:32 +0200

    third assignment in from 94 to 101 assignments

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
-let p = document.querySelector("p");
-p.remove();
-let div = document.querySelector("div");
-
-let befor = document.createElement("div");
-befor.className = "start";
-befor.setAttribute("title", "start element");
-befor.setAttribute("data-value", "start");
-div.before(befor.cloneNode(true));
-let after = document.createElement("div");
-after.className = "end";
-after.setAttribute("title", "end element");
-after.setAttribute("data-value", "end");
-div.after(after.cloneNode(true))
+let span = document.querySelector("span");
+let hello = span.nextSibling;
+let comment = hello.nextSibling;
+let ourWord = comment.nextSibling;
+console.log(ourWord)
