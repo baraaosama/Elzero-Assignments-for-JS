3e42c371c1fd82232be0bf32f9145f20d5f42ebc
baraa osama <baraaosama038@gmail.com>
2022-07-24 09:04:16 +0200

    first assignment in from 102 to 110 assignments

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-let span = document.querySelector("span");
-let hello = span.nextSibling;
-let comment = hello.nextSibling;
-let ourWord = comment.nextSibling;
-console.log(ourWord)
+let popup = document.querySelector("div");
+let btn = document.querySelector("button");
+popup.style.cssText = "background-color:#d6d6c2;width:500px;height:150px;display:none;justify-content:center;align-items:center;flex-direction:column;border:1px solid #ffb3b3;position:relative;top:100px;left:450px"
+btn.style.cssText = "position:absolute;top:-8px;right:-8px;border-radius:50%;width:25px;height:25px;background-color:red;border:none;color:white;";
+
+window.onload = setTimeout(function () {
+    popup.style.display = "flex";
+}, 5000)
+btn.onclick = function () {
+    popup.style.display = "none";
+}
