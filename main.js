776cc9609c5b79bc6ce2dad8e5a3051ca3ef7b5a
baraa osama <baraaosama038@gmail.com>
2022-07-24 09:53:34 +0200

    4th assignment in from 102 to 110 assignments

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
-let value = prompt("Print Number From – To");
-count = 0;
-for (i = 0; i < value.length; i++) {
-    if (value[i] === "-") {
-        break;
-    }
-    count += 1;
-}
-let firstNumber = Number(value.slice(0, count));
-
-let secondNumber = Number(value.slice(count += 1, value.length));
-
-if (firstNumber > secondNumber) {
-    for (i = secondNumber; i < firstNumber + 1; i++) {
-        console.log(i);
-    }
-} else {
-    for (i = firstNumber; i < secondNumber + 1; i++) {
-        console.log(i);
+let div = document.querySelector("div");
+function countDown() {
+    div.innerHTML -= 1;
+    if (div.innerHTML == "0") {
+        clearInterval(counter);
     }
 }
-
-
+let counter = setInterval(countDown, 1000);
