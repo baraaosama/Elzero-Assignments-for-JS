91fc58925d4a3dcfe522f905fead7a58d383c7ae
baraa osama <baraaosama038@gmail.com>
2022-07-24 09:43:14 +0200

    second assignment in from 102 to 110 assignments

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-let popup = document.querySelector("div");
-let btn = document.querySelector("button");
-popup.style.cssText = "background-color:#d6d6c2;width:500px;height:150px;display:none;justify-content:center;align-items:center;flex-direction:column;border:1px solid #ffb3b3;position:relative;top:100px;left:450px"
-btn.style.cssText = "position:absolute;top:-8px;right:-8px;border-radius:50%;width:25px;height:25px;background-color:red;border:none;color:white;";
-
-window.onload = setTimeout(function () {
-    popup.style.display = "flex";
-}, 5000)
-btn.onclick = function () {
-    popup.style.display = "none";
+let value = prompt("Print Number From – To");
+count = 0;
+for (i = 0; i < value.length; i++) {
+    if (value[i] === "-") {
+        break;
+    }
+    count += 1;
 }
+let firstNumber = Number(value.slice(0, count));
+
+let secondNumber = Number(value.slice(count += 1, value.length));
+
+if (firstNumber > secondNumber) {
+    for (i = secondNumber; i < firstNumber + 1; i++) {
+        console.log(i);
+    }
+} else {
+    for (i = firstNumber; i < secondNumber + 1; i++) {
+        console.log(i);
+    }
+}
+
+
