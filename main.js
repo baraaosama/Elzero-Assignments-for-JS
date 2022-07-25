12e05b18dbe8a314f6bccbb57db92c93ab23ec46
baraa osama <baraaosama038@gmail.com>
2022-07-25 13:10:43 +0200

    first assignment in from 115 to 122 assignments

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
-let chosen = 3;
+let myNumbers = [1, 2, 3, 4, 5];
 
-let myFriends = [
-    { title: "Osama", age: 39, available: true, skills: ["HTML", "CSS"] },
-    { title: "Ahmed", age: 25, available: false, skills: ["Python", "Django"] },
-    { title: "Sayed", age: 33, available: true, skills: ["PHP", "Laravel"] },
-];
+// Write Your Destructuring Assignment Here
+let [a, , , , e] = myNumbers;
 
-let [personOne, personTwo, personThree] = myFriends;
-if (chosen == 1) {
-    ({ title, age, available: a, skills: [, m] } = personOne);
-} else if (chosen == 2) {
-    ({ title, age, available: a, skills: [, m] } = personTwo);
-} else {
-    ({ title, age, available: a, skills: [, m] } = personThree);
-}
-console.log(title);
-console.log(age);
-if (a == true) {
-    console.log("available");
-} else {
-    console.log("not available");
-}
-console.log(m);
+console.log(a * e); // 5
