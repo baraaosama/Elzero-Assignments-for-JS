e143ea8ab1123f297420a9d8c04dbfc04fb4aa11
baraa osama <baraaosama038@gmail.com>
2022-07-25 13:20:33 +0200

    third assignment in from 115 to 122 assignments

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-let mySkills = ["HTML", "CSS", "JavaScript", ["PHP", "Python", ["Django", "Laravel"]]];
+let arr1 = ["Ahmed", "Sameh", "Sayed"];
+let arr2 = ["Mohamed", "Gamal", "Amir"];
+let arr3 = ["Haytham", "Shady", "Mahmoud"];
+let newarr = [arr1, arr3];
 
+let [[c, ,], [, a, b]] = newarr;
+
+// Play With Arrays To Prepare For Destructuring
 // Write Your Destructuring Assignment Here
-let [a, b, c, [d, e, [f, g]]] = mySkills;
 
-console.log(`My Skills: ${a}, ${b}, ${c}, ${d}, ${e}, ${f}, ${g}`);
+console.log(`My Best Friends: ${a}, ${b}, ${c}`);
 
-// My Skills: HTML, CSS, JavaScript, PHP, Python, Django, Laravel
+// My Best Friends: Shady, Mahmoud, Ahmed
