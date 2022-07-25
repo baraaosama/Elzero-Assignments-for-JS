e85ab9bf7aad284181061520dc428d270759f6b7
baraa osama <baraaosama038@gmail.com>
2022-07-25 13:06:13 +0200

    Destructing challenge solution

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-let div = document.querySelector("div");
-function countDown() {
-    div.innerHTML -= 1;
-    if (div.innerHTML == "0") {
-        clearInterval(counter);
-    }
+let chosen = 3;
+
+let myFriends = [
+    { title: "Osama", age: 39, available: true, skills: ["HTML", "CSS"] },
+    { title: "Ahmed", age: 25, available: false, skills: ["Python", "Django"] },
+    { title: "Sayed", age: 33, available: true, skills: ["PHP", "Laravel"] },
+];
+
+let [personOne, personTwo, personThree] = myFriends;
+if (chosen == 1) {
+    ({ title, age, available: a, skills: [, m] } = personOne);
+} else if (chosen == 2) {
+    ({ title, age, available: a, skills: [, m] } = personTwo);
+} else {
+    ({ title, age, available: a, skills: [, m] } = personThree);
 }
-let counter = setInterval(countDown, 1000);
+console.log(title);
+console.log(age);
+if (a == true) {
+    console.log("available");
+} else {
+    console.log("not available");
+}
+console.log(m);
