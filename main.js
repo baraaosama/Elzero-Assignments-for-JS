dd115304cb7caaf1c57c03d6321a0f7cbe54bbf1
baraa osama <baraaosama038@gmail.com>
2022-07-27 01:41:45 +0200

    map and set challenge

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-let theNumber = 100020003000;
-
-console.log([...new Set(theNumber.toString())].filter((e) => e != 0).map((e) => +e).join(""));
+let n1 = [10, 30, 10, 20];
+let n2 = [30, 20, 10];
+console.log([...n1, ...n2].length * Math.max(...n2)) //210
