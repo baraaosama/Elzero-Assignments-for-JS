4227a2c61d8436a1935d8827520bc9aa0390e19f
baraa osama <baraaosama038@gmail.com>
2022-07-27 14:23:44 +0200

    6th assignment in from 134 to 146 assignments

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-let date1 = "25/10/1982";
-let date2 = "25 - 10 - 1982";
-let date3 = "25 10 1982";
-let date4 = "25 10 82";
+let url1 = 'elzero.org';
+let url2 = 'http://elzero.org';
+let url3 = 'https://elzero.org';
+let url4 = 'https://www.elzero.org';
+let url5 = 'https://www.elzero.org:8080/articles.php?id=100&cat=topics';
 
-let re = /\d+\/?\s?-?\s?\d+\/?\s?-?\s?\d+/ig
+let re = /(https?:\/\/)?(www.)?\w+.org(:8080\/articles.php\?id=100&cat=topics)?/ig
 
-console.log(date1.match(re)); // "25/10/1982"
-console.log(date2.match(re)); // "25 - 10 - 1982"
-console.log(date3.match(re)); // "25 10 1982"
-console.log(date4.match(re)); // "25 10 82"
+console.log(url1.match(re));
+console.log(url2.match(re));
+console.log(url3.match(re));
+console.log(url4.match(re));
+console.log(url5.match(re));
